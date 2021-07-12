2f6234f382f0962aa5dd3635f7838044c0bd4fff
NatalmanNahm <natalnahm@gmail.com>
2021-07-12 16:53:12 -0500

    Initial Commit

---
+++ PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Natalman Nahm on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
