dc4d8410bd6b7fde0fb1051562cf39e617cbdd1b
NatalmanNahm <natalnahm@gmail.com>
2021-07-14 17:46:24 -0500

    Added new image buttons with a new screen.

--- PitchPerfect/ContentView.swift
+++ PitchPerfect/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            VStack{
+                
+                Button(action: {
+                    print("Hello")
+                }) {
+                    Image("record")
+                        .padding()
+                        .frame(width: 120, height: 120)
+                        .background(Color.green)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color.black)
+                        .foregroundColor(.black)
+                        .shadow(color: .green, radius: 5)
+                }
+                
+                Text("Tap to record")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                NavigationLink(
+                    destination: playback(),
+                    label: {
+                        Image("stop")
+                            .padding()
+                            .frame(width: 65, height: 65)
+                            .background(Color.green)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color.black)
+                            .foregroundColor(.black)
+                            .shadow(color: .green, radius: 5)
+                        
+                    })
+                
+            }
+        }
+       
     }
 }
 
---
+++ PitchPerfect/Playback.swift
@@ -0,0 +1,20 @@
+//
+//  playback.swift
+//  PitchPerfect
+//
+//  Created by Natalman Nahm on 7/12/21.
+//
+
+import SwiftUI
+
+struct playback: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct playback_Previews: PreviewProvider {
+    static var previews: some View {
+        playback()
+    }
+}
